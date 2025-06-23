6559cd8773102f6210aa7f229b37e32491d382ec
Bartosz-Biesaga <bartosz.biesaga123@gmail.com>
2025-06-23 18:45:48 +0200

    Added chronological data splitting

--- process_data.py
+++ process_data.py
@@ -1,6 +1,7 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+import random
 
 # łączy przekazane argumenty dla modelu we wskazanym zakresie lat. Oczekuje 
 # słowników w postaci {(rok, miesiąc): [lista argumentów dla modelu]}
@@ -24,14 +25,37 @@ def add_target_inflations(model_args_dict, target_inflation_dict):
 # Zwraca dane podzielone na uczące i testowe ze znormalizowaną listą 
 # argumentów dla modelu. Oczekuje słownika postaci{(rok, miesiąc): 
 # ([lista argumentów dla modelu], docelowa wartość inflacji}
-def normalize_and_split_data(model_args_and_inflation_dict):
+def normalize_and_split_data(model_args_and_inflation_dict, test_size=0.2):
     dict_values = model_args_and_inflation_dict.values()
     model_args_list = [value[0] for value in dict_values]
     inflation_list = [value[1] for value in dict_values]
     standard_scaler = StandardScaler()
     normalized_model_args_list = standard_scaler.fit_transform(model_args_list)
     X_train, X_test, y_train, y_test = train_test_split(normalized_model_args_list,
-        inflation_list, test_size=0.2, random_state=123)
+        inflation_list, test_size=test_size, random_state=123)
+    return X_train, X_test, y_train, y_test
+
+# Zwraca dane podzielone na uczące i testowe ze znormalizowaną listą 
+# argumentów dla modelu. Oczekuje słownika postaci{(rok, miesiąc): 
+# ([lista argumentów dla modelu], docelowa wartość inflacji}. Gwarantuje zachowanie
+# kolejności chronologicznej danych
+def normalize_and_split_data_chronologically(model_args_and_inflation_dict, test_size=0.2):
+    dict_values = model_args_and_inflation_dict.values()
+    model_args_list = [value[0] for value in dict_values]
+    inflation_list = [value[1] for value in dict_values]
+    standard_scaler = StandardScaler()
+    normalized_model_args_list = standard_scaler.fit_transform(model_args_list)
+    X_train = []
+    X_test = []
+    y_train = []
+    y_test = []
+    for args, inflation in zip(normalized_model_args_list, inflation_list):
+        if random.random() < test_size:
+            y_train.append(args)
+            y_test.append(inflation)
+        else:
+            X_train.append(args)
+            X_test.append(inflation)
     return X_train, X_test, y_train, y_test
 
 # if __name__ == "__main__":
